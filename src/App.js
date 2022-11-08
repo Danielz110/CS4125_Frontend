30c4a531a6d0c8c4f1338ad16df67b5e3fcf574a
Danielz110 <64483972+Danielz110@users.noreply.github.com>
2022-11-08 18:27:15 +0000

    Initial Setup of repository
    Initial Setup of repository
    -Navbar Done
    -Home Page started

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './layout/Navbar';
+import React from 'react'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
